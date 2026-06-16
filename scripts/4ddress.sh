0e35b96b91fa3abf01c0898deafafb27ce84148d
zychen <zeyu.chen07@gmail.com>
2026-06-16 18:00:03 +0800

    update

---
+++ scripts/4ddress.sh
@@ -0,0 +1,7 @@
+python prepare_4ddress.py \
+    --root_dir .datasets/4ddress \
+    --output_dir ./train_data/4ddress_lhm \
+    --model_path ./pretrained_models \
+    --subject 00123 \
+    --mode all \
+    --build_meta
